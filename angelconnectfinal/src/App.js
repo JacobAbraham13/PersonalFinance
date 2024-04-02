72ba2ee350f251a6dc61ffe7152fdfe957c415c6
Thaanvi Arun <thaanvi.a@gmail.com>
2024-04-01 20:22:07 -0500

    merge hero and home page

---
+++ angelconnectfinal/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroPage from './HeroPage';
+
+const App = () => {
+  return (
+    <div>
+      <HeroPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ angelconnectfinal/src/HeroPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeroPage.css'; // Import CSS file for styling
+
+const HeroPage = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+      <h1>CONNECTING<span className="big-word"> ANGEL INVESTORS</span></h1>
+      <h1>WITH <span className="big-word"> MINORITY </span> OWNED BUSINESSES</h1>
+        <button>GET MATCHED</button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroPage;
